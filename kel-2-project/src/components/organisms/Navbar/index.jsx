d5f3053790e7f10c35101c69aa52e24d92def2d9
Adisupra207 <adisupra207@gmail.com>
2023-03-08 14:57:48 +0700

    navbar ref changes

--- kel-2-project/src/components/organisms/Navbar/index.jsx
+++ kel-2-project/src/components/organisms/Navbar/index.jsx
@@ -31,7 +31,7 @@ export default function NaviBar() {
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="/home">Home</Nav.Link>
                 <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/contact">List Unit</Nav.Link>                
+                <Nav.Link href="/listunit">List Unit</Nav.Link>                
               </Nav>
               <Form className="d-flex">
                 <Button
