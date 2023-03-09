aa09be2aeb60a4196d28c83403d785e43ea9673f
Adisupra207 <adisupra207@gmail.com>
2023-03-09 12:45:46 +0700

    Menambahkan list unit

--- kel-2-project/src/components/organisms/index.js
+++ kel-2-project/src/components/organisms/index.js
@@ -1,5 +1,6 @@
 import Navbar from './Navbar';
 import Header from './Header';
 import Footer from './Footer';
+import Kartu from './Card';
 
-export { Navbar, Header, Footer }
+export { Navbar, Header, Footer, Kartu }
